d93b5018b4a75a6d8235da3a3ac12c936024bba7
aslikeyou <pekhota.alex@gmail.com>
2015-04-26 23:53:24 +0300

    asdasda

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,6 +14,7 @@ class Kernel extends ConsoleKernel {
 		'App\Console\Commands\Inspire',
 		'App\Console\Commands\VkPublisher',
 		'App\Console\Commands\VkRemoveUsers',
+		'App\Console\Commands\DropboxTest',
 	];
 
 	/**
---
+++ config/dropbox.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'app_key'       => env('DROPBOX_APP_KEY'),
+    'app_secret'    => env('DROPBOX_APP_SECRET'),
+    'access_token'  => env('DROPBOX_ACCESS_TOKEN'),
+];
